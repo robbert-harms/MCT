202ba0a287bcb1487615b7f0117e831dfc93ba9c
Robbert Harms <robbert@xkls.nl>
2017-09-12 14:05:52 +0200

    Updates to docs

--- docs/conf.py
+++ docs/conf.py
@@ -262,11 +262,11 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {
-   '**': ['about.html',
-          'navigation.html',
-          'searchbox.html']
-}
+# html_sidebars = {
+#    '**': ['about.html',
+#           'navigation.html',
+#           'searchbox.html']
+# }
 
 # Additional templates that should be rendered to pages, maps page names
 # to template names.
